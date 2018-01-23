d1b2476259f727092ee83a206badb1015f2b4918
zolangi <zolangiramirez@gmail.com>
2018-01-23 14:11:06 -0800

    Upload skill

---
+++ questions.js
@@ -0,0 +1,130 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US": [
+        {
+            "What is the name of the main character?":[
+                "Clarice Starling",
+                "Berenice Star",
+                "Joanna Darling",
+                "Chloe Daze",
+            ]
+        },
+        {
+            "Who is Clarice Starling?":[
+                "FBI trainee",
+                "FBI agent",
+                "Detective",
+                "FBI Behavioral analyst"
+            ]
+        },
+        {
+            "What city is Clarice from?":[
+                "West Virginia",
+                "Virginia",
+                "South Carolina",
+                "North Carolina"
+            ]
+        },
+        {
+            "Where is Clarice training to become an FBI agent?":[
+                "Quantico",
+                "Pentagon",
+                "Washington DC",
+                "Oregon"
+            ]
+        },
+        {
+            "Who is Jack Crawford?":[
+                "Behavioral Sciencetist",
+                "Behavioral Analyst",
+                "Crisis Negotiator",
+                "Hostage Rescuer"
+            ]
+        },
+        {
+            "Who is Hannibal Lecter?":[
+                "Imprisoned Psychiatrist",
+                "Psychiatrist",
+                "Imprisoned Philosopher",
+                "Philosopher"
+            ]
+        },
+        {
+            "Why was Hannibal Lecter imprisoned?":[
+                "Cannibalized victims",
+                "Skinned victims",
+                "Poisoned victims",
+                "Burned victims"
+            ]
+        },
+        {
+            "How long has Hannibal Lecter been imprisoned for?":[
+                "8 years",
+                "10 years",
+                "5 years",
+                "15 years"
+            ]
+        },
+        {
+            "Who is a Buffalo Bill?":[
+                "Serial Killer who skins his victims",
+                "Serial Killer who burns his victims",
+                "Serial Killer who eats his victims",
+                "Serial Killer who poisons his victims"
+            ]
+        },
+        {
+            "How many victims has Buffalo Bill claimed?":[
+                "6",
+                "5",
+                "7",
+                "3"
+            ]
+        },
+        {
+            "Who is Frederica Bimmel?":[
+                "Buffalo Bills 1st victim",
+                "Buffalo Bills 2nd victim",
+                "Buffalo Bills 5th victim",
+                "Buffalo Bills 4th victim"
+            ]
+        },
+        {
+            "What is Buffalo Bill's real name?":[
+                "James Gumb",
+                "James Gumbo",
+                "Jesse Gumb",
+                "Jesse Gumbo"
+            ]
+        },
+        {
+            "Who is Catherine Martin?":[
+                "A U.S. Senator's Daughter",
+                "A U.S. House Representative's Daughter",
+                "A U.S. FBI Agent's Daughter",
+                "U.S. President's Daughter"
+            ]
+        },
+        {
+            "Where is Hannibal Lecter last seen in the movie?":[
+                "Bahamas",
+                "Hawaii",
+                "Mexico",
+                "Rio de Janiero"
+            ]
+        },
+        {
+            "What is the last thing Hannibal says to Clarice?":[
+                "Having an old friend for dinner",
+                "Having dinner with an old friend",
+                "Having an old friend over for dinner",
+                "Meeting an old friend for dinner"
+            ]
+        }
+    ]
+};
